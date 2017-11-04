8a66c200e9beb0238d39a3cc87b4ec83c2eec030
irismanriquezc <imanriquez@csumb.edu>
2017-11-04 16:59:19 -0700

    "k"

--- app/src/main/java/com/example/wicky/breakout/MainActivity2.java
+++ app/src/main/java/com/example/wicky/breakout/MainActivity2.java
@@ -1,7 +1,9 @@
 package com.example.wicky.breakout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity2 extends AppCompatActivity {
@@ -12,6 +14,11 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
     }
 // code in here
+    public void Hurr(View view){
+    Intent DaHurricane = new Intent(this, hurricane.class)
+
+    startActivity(DaHurricane)
+    }
 
 
 
