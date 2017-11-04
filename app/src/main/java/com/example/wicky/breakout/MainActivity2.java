c1ba32d54406d3c8d7ef8e27c08e00a641f4b85e
Enrique Pesqueda Mosqueda <enrique7831@gmail.com>
2017-11-04 12:11:13 -0700

    This is the latest break out

---
+++ app/src/main/java/com/example/wicky/breakout/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.wicky.breakout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+
+
+}
