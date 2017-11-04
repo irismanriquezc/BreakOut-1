bc58a57a9e96d578bf894564ca46062f1f7e7f1f
Enrique Pesqueda Mosqueda <enrique7831@gmail.com>
2017-11-04 15:50:26 -0700

    This is the latest break out

--- app/src/main/java/com/example/wicky/breakout/MainActivity.java
+++ app/src/main/java/com/example/wicky/breakout/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.wicky.breakout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public void SurvivalGuide(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
 
 
 
 }
+
+
+
+
+
